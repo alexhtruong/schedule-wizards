e6b8f4d73e9722c651fc3fe2d0db68940526e0e9
colinmhassett <163110923+colinmhassett@users.noreply.github.com>
2025-05-10 22:57:40 -0700

    made basic create_department function

---
+++ src/api/routers/departments.py
@@ -0,0 +1,74 @@
+from unittest.util import strclass
+from fastapi import APIRouter, Query, HTTPException
+from pydantic import BaseModel, Field
+from typing import List, Optional
+import sqlalchemy
+from src import database as db
+
+router = APIRouter(prefix="/departments", tags=["departments"])
+
+class Professor(BaseModel):
+    name: str
+    professor_id: int
+
+class Course(BaseModel):
+    course_id: int
+    name: str
+    department: str
+    professors: List[Professor]
+
+class Department(BaseModel):
+    department_id: int
+    name: str
+    abbrev: str
+    school_id: int
+
+class DepartmentCreate(BaseModel):
+    department_id: int
+    name: str
+    abbrev: str
+    school_id: int
+    
+
+@router.post("/")
+async def create_department(department: DepartmentCreate):
+    """Create a new department."""
+    with db.engine.begin() as connection:
+        department = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT 1
+                FROM departments
+                WHERE departments.name = :name AND departments.school_id = :school_id
+                """
+            ),
+            {
+                "name": department.name,
+                "school_id": department.school_id
+            }
+        ).first()
+
+        if department is not None:
+            raise HTTPException(
+                status_code=400,
+                detail="department already exists!"
+            )
+
+        connection.execute(
+            sqlalchemy.text(
+                """
+                INSERT INTO departments
+                (name,
+                abbrev,
+                school_id)
+                VALUES
+                (:name, :abbrev, :school_id)
+                RETURNING id
+                """
+                ),
+            {
+                "name": department.name,
+                "abbrev": department.abbrev,
+                "school_id": department.school_id
+            }
+            )
