eb7e1ff9a4ef303cee5f1afca4251ce3fbf5be51
colinmhassett <163110923+colinmhassett@users.noreply.github.com>
2025-05-25 20:42:29 -0700

    made it so can list schools

--- src/api/routers/schools.py
+++ src/api/routers/schools.py
@@ -1,8 +1,8 @@
 from fastapi import APIRouter, HTTPException
 import sqlalchemy
 from src import database as db
-from src.api.routers.models import SchoolCreate
-
+from src.api.routers.models import SchoolCreate, School
+from typing import List, Any, Dict
 router = APIRouter(prefix="/schools", tags=["schools"])
 
 @router.post("/")
@@ -52,3 +52,51 @@ async def create_school(school: SchoolCreate):
         school_id = result.scalar_one()
         return {"id": str(school_id), "message": "School created successfully"}
 
+    
+
+@router.get("/")
+async def list_schools(limit: int = 10, offset: int = 0) -> Dict[str, Any]:
+    """List available schools."""
+    with db.engine.begin() as connection:
+        
+        total_result = connection.execute(
+            sqlalchemy.text(
+                "SELECT COUNT(*) FROM school"
+            )
+        ).scalar_one()
+
+
+        schools = connection.execute(
+            sqlalchemy.text(
+                """
+                SELECT id, name, city, state, country
+                FROM school
+                LIMIT :limit
+                OFFSET :offset
+                """
+            ),
+            {
+                "limit": limit,
+                "offset": offset
+            }
+        ).all()
+
+
+        schools_list = []
+
+        for row in schools:
+            Id = row.id
+            name = row.name
+            city = row.city
+            state = row.state
+            country = row.country
+            schools_list.append(School(id = Id, name =name, city=city, state = state, country = country))
+
+
+        return {
+            "total": total_result,
+            "schools": schools_list
+        }
+
+
+        
